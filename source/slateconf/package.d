d74b4aa1175db0314f209e47fc94917485795591
mostypc123 <mostypc123@redroselinux.org>
2026-09-13 19:54:58 +0200

    init: slate-conf

---
+++ source/slateconf/package.d
@@ -0,0 +1,45 @@
+module slateconf;
+public import parser;
+
+private union ConfigNodeValue {
+  string s;
+  string[] sa;
+}
+
+struct ConfigNode {
+  string group;
+  string[] keys;
+  ConfigNodeValue value;
+  bool is_arr_or_s;
+}
+
+class SlateConfParseError : Exception {
+  this(string msg, string file = __FILE__, size_t line = __LINE__) {
+    super(msg, file, line);
+  }
+}
+
+class SlateConfOpIndexError : Exception {
+  this(string msg, string file = __FILE__, size_t line = __LINE__) {
+    super(msg, file, line);
+  }
+}
+
+struct SlateConf {
+  ConfigNode[] values;
+
+  import std.variant : Algebraic;
+  Algebraic!(string, string[]) opIndex(string val) {
+    import std.array : join;
+
+    foreach (key; values) {
+      auto wgroup = key.keys.join(".");
+      if (wgroup == val) {
+        if (key.is_arr_or_s) return Algebraic!(string, string[])(key.value.sa);
+        else return Algebraic!(string, string[])(key.value.s);
+      }
+    }
+
+    throw new SlateConfOpIndexError("no such value " ~ val);
+  }
+}
---
+++ source/slateconf/parser.d
@@ -0,0 +1,84 @@
+module parser;
+import slateconf;
+
+class NotImplementedError : Exception {
+  this(string msg, string file = __FILE__, size_t line = __LINE__) {
+    super(msg, file, line);
+  }
+}
+
+SlateConf parse(string config) {
+  import std.string : splitLines, split, strip, startsWith, endsWith;
+  import std.conv : to;
+
+  SlateConf result;
+
+  long loc = 1;
+  foreach (line; splitLines(config)) {
+    line = strip(line);
+    if (line == "") continue;
+
+    ConfigNode line_result;
+
+    if (line[0] == '{') {
+      // TODO: groups
+      throw new NotImplementedError("groups are not yet implemented");
+    } else {
+      auto parts = line.split("::");
+      foreach (ref part; parts) part = strip(part);
+
+      bool list = parts[0].startsWith("[");
+      if (list && !parts[0].endsWith("]")) {
+        throw new SlateConfParseError(
+          "[ in list declaration not closed : " ~ to!string(loc)
+        );
+      }
+
+      foreach (i, part; parts) {
+        part = strip(part);
+        if (part.startsWith("--")) continue;
+        else if (parts.length == 1) {
+          throw new SlateConfParseError(
+            "key " ~ parts[0] ~ " cannot be used by itself : " ~ to!string(loc)
+          );
+        }
+
+        if (i == cast(ptrdiff_t) parts.length - 1) {
+          if (list) {
+            line_result.value.sa = parts[1 .. $];
+            line_result.is_arr_or_s = true;
+          } else {
+            line_result.value.s = part;
+            line_result.is_arr_or_s = false;
+          }
+        } else {
+          line_result.keys ~= part;
+        }
+      }
+    }
+
+    result.values ~= line_result;
+    loc++;
+  }
+
+  return result;
+}
+
+SlateConf parseFile(string path) {
+  import std.file : readText;
+  return parse(readText(path));
+}
+
+unittest {
+  import std.stdio;
+
+  SlateConf result;
+  try {
+    result = parseFile("example.slate");
+  } catch (SlateConfParseError e) {
+    writeln(e.msg);
+    assert(false);
+  }
+
+  assert(result["key"] == "value");
+}
